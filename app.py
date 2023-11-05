f60be716156e03861f34a03b5c31ef1de5784d43
VishalSehgal525 <vsehgal_be20@thapar.edu>
2023-11-05 12:13:03 +0530

    Add

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import librosa
 import torch
 import soundfile as sf
 import numpy as np
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from transformers import pipeline
 from gtts import gTTS
 import pandas as pd
 import numpy as np
@@ -24,12 +24,11 @@ import re
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
+
 app = Flask(__name__)
 app.secret_key = 'dc9a0187c5297e94c26cdd32ffb3266eda502bc49a2874cb77961707ecda021f'
 app.debug = True
-asr_processor = WhisperProcessor.from_pretrained("openai/whisper-small")
-asr_model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
-asr_model.config.forced_decoder_ids = None
+pipe = pipeline("automatic-speech-recognition", model="openai/whisper-small")
 
 
 @app.route('/')
@@ -44,8 +43,12 @@ def upload_audio():
             audio_file.save("static/music/recorded_audio.wav")
             text="hello shivam"
             tts=gTTS(text=text,lang="en",tld="co.in")
-            tts.save("static/music/text.mp3")            
-            
+            tts.save("static/music/text.mp3")  
+            audio_path = "static/music/recorded_audio.wav"
+            sample_rate = 16000
+            audio_input, sample_rate = librosa.load(audio_path, sr=sample_rate)
+            transcribe = pipe(audio_input)
+            print(transcribe['text'])
             
         
 
